57d606961dbf61dce4f52b2f732fac19b9d7d653
lex <httpooo@yandex.ru>
2021-05-22 21:33:47 -0400

    v2

--- Program.cs
+++ Program.cs
@@ -8,11 +8,9 @@ namespace tasks
     {
         static void Main(string[] args)
         {
-            /*
             lesson1.enquete();
-            Console.WriteLine("Your bmi = " + lesson1.bmi() + '\r\n');
-            Console.WriteLine("Distance = " + lesson1.euclideanDist(0,0,1,1).ToString("f2") + '\r\n');
-            */
+            Console.WriteLine("Your bmi = " + lesson1.bmi() + "\r\n");
+            Console.WriteLine("Distance = " + lesson1.euclideanDist(0,0,1,1).ToString("f2") + "\r\n");
             lesson1.swapVariables(3, 5);
             lesson1.swapVariables2(7, 8);
             lesson1.task5("Alexander", "Prikhodov", "Moscow");
--- tasks/lesson1.cs
+++ tasks/lesson1.cs
@@ -3,6 +3,26 @@ using System;
 //Author: Prikhodov Alexander.
 
 namespace tasks {
+    public class dot{
+        public double x, y;
+        public void enter(){
+            Console.WriteLine("Enter a dot in format \"x y\"\r\n");
+            string[] res = Console.ReadLine().Split(' ');
+            if (res.Length != 2) {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+            x = double.Parse(res[0]);
+            y = double.Parse(res[1]);
+        }
+        public void subtract(dot d){
+            x -= d.x;
+            y -= d.y;
+        }
+        public double eucledeanNorm(){
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+    }
 
     public class usefulMethods{
         public static string enter(string message){
@@ -21,10 +41,12 @@ namespace tasks {
                 Console.Write(line + "\r\n");
             }
         }
+
     }
 
     public class lesson1{
         public static void enquete(){
+            Console.WriteLine(">> enquette function:\r\n");
             string name = usefulMethods.enter("Please, enter your name");
             string surname = usefulMethods.enter("Please, enter your surname");
             int age = Int32.Parse(usefulMethods.enter("Enter your age"));
@@ -34,6 +56,7 @@ namespace tasks {
         }
 
         public static float bmi(){
+            Console.WriteLine(">> bmi function:\r\n");
             float m = float.Parse(usefulMethods.enter("Enter your mass in kg"));
             float h = float.Parse(usefulMethods.enter("Enter your height in cm")) / 100f;
             float ind = m / (h * h);
@@ -41,10 +64,18 @@ namespace tasks {
         }
         
         public static double euclideanDist(double x1, double y1, double x2, double y2){
-            return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            Console.WriteLine(">> euclideanDist function:\r\n");
+            Console.WriteLine("Enter dots:");
+            //string[] a = Console.ReadLine().Split(' ');
+            dot d1 = new dot(), d2 = new dot();
+            d1.enter();
+            d2.enter();
+            d1.subtract(d2);
+            return d1.eucledeanNorm();
         }
 
         public static void swapVariables(int a, int b){
+            Console.WriteLine(">> swapVariables function:\r\n");
             int c = a;
             Console.WriteLine($"SwapVariables:\r\nBefore swap: a = {a}, b = {b}");
             a = b;
@@ -53,6 +84,7 @@ namespace tasks {
         }
 
         public static void swapVariables2(int a, int b){
+            Console.WriteLine(">> swapVariables2 function:\r\n");
             Console.WriteLine($"SwapVariables2:\r\nBefore swap: a = {a}, b = {b}");
             a = a ^ b;
             b = a ^ b;
@@ -61,6 +93,7 @@ namespace tasks {
         }
 
         public static void task5(string name, string surname, string city){
+            Console.WriteLine(">> task5 function:\r\n");
             string line = $"Name: {name}, surname: {surname}, city: {city}";
             var pos = Console.GetCursorPosition();
             var size = Console.WindowWidth;
